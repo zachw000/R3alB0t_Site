e9126ee07829d71e9cd4206b966c1460d3204dc5
Zachary Waldron <wewingames222@gmail.com>
2016-09-17 18:53:51 -0700

    PHP Setup

--- index.php
+++ index.php
@@ -1,12 +1,13 @@
 <?php
 include 'templates/header.php';
+include 'templates/wrapper.php';
+
 $DIRS_DOWN = 0;
 
 createHeader("R3alB0at Beta", $DIRS_DOWN);
 bodyMain("class='page-container-bg-solid page-md'");
 ?>
 	<?php
-	include 'templates/wrapper.php';
 	wrapper("home", $DIRS_DOWN);
 	appendScripts($DIRS_DOWN);
 	?>
--- templates/topbar.php
+++ templates/topbar.php
@@ -9,6 +9,45 @@ function generateTopBar($layout, $prependData) {
 					<img src="<?php echo $prependData; ?>assets/logo_color.png" alt="R3alB0t" class="logo-default" />
 				</a>
 			</div>
+			<a href="javascript:;" class="menu-toggler"></a>
+			<div class="top-menu">
+				<ul class="nav navbar-nav pull-right">
+					<li class="dropdown dropdown-extended dropdown-tasks dropdown-dark" id="header_task_bar">
+						<a href="javascript:;" class="dropdown-toggle" data-toggle="dropdown" data-hover="dropdown" data-close-others="true" aria-expanded="false">
+							<i class="icon-bell"></i>
+							<?php
+							// temporary value, at least until oauth and databases is implemented
+							$alerts = false;
+							$alerts_total = 2;
+							if ($alerts) {
+							?>
+							<span class="badge badge-default"><?php echo $alerts_total; ?></span>
+						</a>
+							<ul class="dropdown-menu">
+								<li class="external">
+									<h3>You have <strong><?php echo $alerts_total; ?> new alerts on your server.</h3>
+									<a href="<?php echo $prependData; ?>alerts?UID=userID">View all</a>
+								</li>
+								<li>
+									<div class="slimScrollDiv" style="position:relative; overflow: hidden; width: auto; height: 300px;">
+										<ul class="dropdown-menu-list scroller" style="height: 300px; overflow: hidden; width: auto;" data-handle-color="#637283" data-initialized="1">
+											<!-- SHOW ALL ALERTS UP TO 9 ITEMS //-->
+										</ul>
+										<div class="slimScrollBar"></div>
+										<div class="slimScrollRail"></div>
+									</div>
+								</li>
+							</ul>
+							<?php
+							} else {
+							?>
+							</a>
+							<?php
+							}
+							?>
+					</li>
+				</ul>
+			</div>
 		</div>
 	</div>
 <?php
